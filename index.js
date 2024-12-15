9a60fd66e93c2fd875c031a8133b8c2d36f02b52
Mario Villagra <mvillagra.lorca@gmail.com>
2024-12-15 18:03:47 -0300

    Add configuracion servidor

--- index.js
+++ index.js
@@ -1,7 +1,26 @@
 const { connection } = require("./database/connection")
+const express = require("express")
+const cors = require("cors")
 
 //Inicializar app
 console.log("Cambio, hola que tal")
 
 //Conectar a la base de datos
 connection()
+
+//Crear servidor Node
+const app = express()
+const puerto = 3900
+
+//Configurar cors
+app.use(cors())
+
+//Convertir body a objeto js
+app.use(express.json())
+
+//crear rutas
+
+//crear servidor y escuchar peticiones
+app.listen(puerto, () => {
+    console.log(`Servidor corriendo en el puerto ${puerto}`)
+})

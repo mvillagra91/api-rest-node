4280211aa7eb0fd0fd0de5ac0f45961c011c89e6
Mario Villagra <mvillagra.lorca@gmail.com>
2024-12-15 18:40:40 -0300

    Add Estructura MVC

--- database/connection.js
+++ database/connection.js
@@ -1,19 +1,19 @@
-const mongose = require("mongoose")
+const mongose = require("mongoose");
 
 const connection = async() => {
     try{
 
-        await mongose.connect("mongodb://localhost:27017/mi_blog")
+        await mongose.connect("mongodb://localhost:27017/mi_blog");
 
-        console.log("Conectado correctamente a la base de datos mi_blog")
+        console.log("Conectado correctamente a la base de datos mi_blog");
         //parametros a pasar dentro de un objeto /Solo en caso de aviso
         //useNewUrlParser: true
         //useUnifiedTopology: true
         //useCreateIndex: true
 
     }catch(error){
-        console.log(error)
-        throw new Error("No se ha podido ingresar a la base de datos")
+        console.log(error);
+        throw new Error("No se ha podido ingresar a la base de datos");
     }
 }
 
--- index.js
+++ index.js
@@ -1,26 +1,45 @@
-const { connection } = require("./database/connection")
-const express = require("express")
-const cors = require("cors")
+const { connection } = require("./database/connection");
+const express = require("express");
+const cors = require("cors");
 
 //Inicializar app
-console.log("Cambio, hola que tal")
+console.log("Cambio, hola que tal");
 
 //Conectar a la base de datos
-connection()
+connection();
 
 //Crear servidor Node
-const app = express()
-const puerto = 3900
+const app = express();
+const puerto = 3900;
 
 //Configurar cors
-app.use(cors())
+app.use(cors());
 
 //Convertir body a objeto js
-app.use(express.json())
+app.use(express.json());
 
 //crear rutas
+app.get("/probando", (req, res) => {
+    console.log("Se ha ejecutado el endpoint probando");
+
+    return res.status(200).json([{
+        curso:"master en react"
+    },
+    {
+        curso:"master en js"
+    }
+        
+    ]
+)})
+
+app.get("/", (req, res) => {
+    console.log("Se ha ejecutado el endpoint probando");
+
+    return res.status(200).send(
+        "<h1> Empezando a crear una api rest con node </h1>"
+)})
 
 //crear servidor y escuchar peticiones
 app.listen(puerto, () => {
-    console.log(`Servidor corriendo en el puerto ${puerto}`)
+    console.log(`Servidor corriendo en el puerto ${puerto}`);
 })
